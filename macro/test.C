c39e084785c7e907cc4c700fb8016eeef7063a00
remotain <remoto@in2p3.fr>
2017-03-02 13:24:09 +0100

    Update file name

--- macro/test.C
+++ macro/test.C
@@ -10,16 +10,30 @@
 	//
 	// Add root file to process
 	//
-	t->Add("$WLS_PATH/examples/output_000176.root");
+	t->Add("$WLS_PATH/examples/output00000186.root");
 	//
 	// Create the loop object
 	//
 	loop * l = new loop();
 	//
+	// Create an instance of the module_pedestal 
+	//
+	module_pedestal * my_module_pedestal = new module_pedestal();
+	my_module_pedestal->set_nsample(100); // set the number of sample used to compute the pedestal
+	//
+	// Create an instance of the module_charge
+	//
+	module_charge * my_module_charge = new module_charge();
+	//
 	// Add modules to the even processing pipeline
 	//
-	l->add_module( new module_pedestal    );
-	l->add_module( new module_charge      );
+	l->add_module( my_module_pedestal );
+	l->add_module( my_module_charge   );
+
+	l->add_module( new module_plot_dump ); 
+	l->add_module( new module_evt_dump );
+
+
 	//
 	// Run the loop
 	//	
