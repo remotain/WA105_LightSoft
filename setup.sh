ac4f46ca099ea10ac216145d139a0af95efbd284
remotain <remoto@in2p3.fr>
2017-03-15 10:31:00 +0100

    Create $WLS_DATA and $WLS_PLOT directory if they do not alreay exist

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export WLS_PATH=$(cd -P $(dirname "${BASH_SOURCE[0]}") > /dev/null && pwd -P)
-export WLS_DATA=${WLS_PATH}/examples/
-export WLS_PLOT=${WLS_PATH}/plots/
+export WLS_DATA=${WLS_PATH}/examples/ && mkdir -p ${WLS_DATA}
+export WLS_PLOT=${WLS_PATH}/plots/ && mkdir -p ${WLS_PLOT}
