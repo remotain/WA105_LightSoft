bee64c5d9e5ac79022565bc2ed6d45901e1e2c35
remotain <remoto@in2p3.fr>
2017-06-23 11:32:59 +0200

    - Remove averaged waveforms, - Add yz view of the event with TH2Poly

--- src/module_evt_display.cc
+++ src/module_evt_display.cc
@@ -22,51 +22,63 @@
 #include <TLine.h>
 #include <TStyle.h>
 #include <TFile.h>
+#include <TTimeStamp.h>
+#include <TH2Poly.h>
 
 void module_evt_display::begin(){
 
 	gROOT->SetBatch(false);
 
-    c_pmt = new TCanvas("PMT Display", "PMT Display");
-    c_pmt->Divide(5,3);	
+    c_pmt = new TCanvas("PMT Display", "PMT Display", 1000, 400);
+    c_pmt->Divide(5,2);	
 
     //c_crt = new TCanvas("CRT Display", "CRT Display");
     //c_crt->Divide(3,2);	
 
 	for( int ch = 0; ch < 5; ch++ ){	
 		_waveform_avg.push_back( new TProfile( TString::Format("waveform_avg_%i", ch ), TString::Format("waveform_avg_%i", ch ), 1000, 0, 4000 ));
-		_waveform_avg[ch]->GetYaxis()->SetRangeUser(0, 4500);
+		_waveform_avg[ch]->GetYaxis()->SetRangeUser(3800, 4010);
 	}
 
 	gStyle->SetOptStat(0);
+	gStyle->SetPalette(53);
 
 }
 
 bool module_evt_display::process( event * evt){
 	
-	Info("process", "Event no.%i, CRT Match %i, CRT Reco %i", evt->get_nevent(), evt->get_crt_daq_match(), evt->get_crt_reco());
+	TTimeStamp evt_t(evt->get_time_stamp());
 	
+	Info("process", "Event no.%i, Time Stamp %s", evt->get_nevent(), evt_t.AsString("l"));
+	Info("process", "CRT Match %i, CRT Reco %i", evt->get_crt_daq_match(), evt->get_crt_reco());
+	Info("process", "Track Z %.2f, tan(theta) %.2f", evt->get_crt_track_param()[0], evt->get_crt_track_param()[1] );		
+	
+	
+	c_pmt->SetTitle( TString::Format("Evt %i, Time %s", evt->get_nevent(), evt_t.AsString("l")) );
+		
 	// PMT waveforms, 5 canvas
 	
 	for( int ch = 0; ch < evt->get_n_channels_active(); ch++ ){	
 		
 		_waveform.push_back( new TH1F( TString::Format("waveform_%i_%i", ch , evt->get_nevent() ), TString::Format("waveform_%i_%i", ch , evt->get_nevent() ), evt->get_waveform(ch)->size(), 0, evt->get_waveform(ch)->size() * evt->get_time_sample() ));
-		_waveform[ch]->GetYaxis()->SetRangeUser(0, 4500);
+		_waveform[ch]->GetYaxis()->SetRangeUser(3800, 4010);
 		
-		for( int i = 0 ; i < evt->get_waveform(ch)->size(); i++) {
+		for( int i = 0 ; i < _waveform[ch]->GetNbinsX(); i++) {
 			_waveform[ch] -> Fill( i * evt->get_time_sample(), evt->get_waveform(ch)->at(i) );		
 			
 			if( ch < 5 ) 
 				_waveform_avg[ch] -> Fill( i * evt->get_time_sample(), evt->get_waveform(ch)->at(i) );		
 		
 		}
+	
 
 		if( ch < 5 ) {
+	
 			c_pmt->cd(ch+1); _waveform[ch] -> Draw("HIST");
 			c_pmt->cd(ch+1)->Modified(); c_pmt->cd(ch+1)->Update();
 
-			c_pmt->cd(ch+1+5); _waveform_avg[ch] -> Draw("");
-			c_pmt->cd(ch+1+5)->Modified(); c_pmt->cd(ch+1+5)->Update();
+			//c_pmt->cd(ch+1+5); _waveform_avg[ch] -> Draw("");
+			//c_pmt->cd(ch+1+5)->Modified(); c_pmt->cd(ch+1+5)->Update();
 		}
 	
 		//std::vector<int> * waveform = evt->get_waveform(ch);
@@ -79,15 +91,15 @@ bool module_evt_display::process( event * evt){
 	// Trigger waveforms, 1 canvas
 	
 	_waveform[5]->SetTitle("CRT Trigger signal");
-	c_pmt->cd(11); _waveform[5] -> Draw("HIST");
-	c_pmt->cd(11)->Modified(); c_pmt->cd(11)->Update();
+	c_pmt->cd(6); _waveform[5] -> Draw("HIST");
+	c_pmt->cd(6)->Modified(); c_pmt->cd(6)->Update();
 	
 	// CRT Waveform, 1 canvas
 	
-	TH1F * a_0 = new TH1F( TString::Format("crt_adc_%i_%i", 0 , evt->get_nevent() ), TString::Format("crt_adc_%i_%i", 0 , evt->get_nevent() ), 32, 0, 32 );
-	TH1F * a_1 = new TH1F( TString::Format("crt_adc_%i_%i", 1 , evt->get_nevent() ), TString::Format("crt_adc_%i_%i", 1 , evt->get_nevent() ), 32, 0, 32 );	
-	TH1F * a_2 = new TH1F( TString::Format("crt_adc_%i_%i", 2 , evt->get_nevent() ), TString::Format("crt_adc_%i_%i", 2 , evt->get_nevent() ), 32, 0, 32 );
-	TH1F * a_3 = new TH1F( TString::Format("crt_adc_%i_%i", 3 , evt->get_nevent() ), TString::Format("crt_adc_%i_%i", 3 , evt->get_nevent() ), 32, 0, 32 );
+	TH1F * a_0 = new TH1F( TString::Format("crt_adc_%i_%i_%i", 0 , evt->get_nevent(), evt->get_time_stamp() ), TString::Format("crt_adc_%i_%i", 0 , evt->get_nevent() ), 32, 0, 32 );
+	TH1F * a_1 = new TH1F( TString::Format("crt_adc_%i_%i_%i", 1 , evt->get_nevent(), evt->get_time_stamp() ), TString::Format("crt_adc_%i_%i", 1 , evt->get_nevent() ), 32, 0, 32 );	
+	TH1F * a_2 = new TH1F( TString::Format("crt_adc_%i_%i_%i", 2 , evt->get_nevent(), evt->get_time_stamp() ), TString::Format("crt_adc_%i_%i", 2 , evt->get_nevent() ), 32, 0, 32 );
+	TH1F * a_3 = new TH1F( TString::Format("crt_adc_%i_%i_%i", 3 , evt->get_nevent(), evt->get_time_stamp() ), TString::Format("crt_adc_%i_%i", 3 , evt->get_nevent() ), 32, 0, 32 );
 	
 	a_0->GetYaxis()->SetRangeUser(0, 4500);
 	
@@ -106,28 +118,28 @@ bool module_evt_display::process( event * evt){
     
 	}
 	
-	c_pmt->cd(12);
+	c_pmt->cd(7);
     
 	a_0->Draw("HIST");
 	a_1->Draw("HIST,Same");
 	a_2->Draw("HIST,Same");
 	a_3->Draw("HIST,Same");
 	
-	c_pmt->cd(12)->BuildLegend();
-	c_pmt->cd(12)->Modified(); c_pmt->cd(12)->Update();
+	c_pmt->cd(7)->BuildLegend();
+	c_pmt->cd(7)->Modified(); c_pmt->cd(7)->Update();
 	
-	TH2F * crt_xy_0 = new TH2F( TString::Format("crt_xy_0_%i", evt->get_nevent() ), TString::Format("crt_xy_0_%i", evt->get_nevent() ), 32, 0, 32, 32, 0, 32 );
-	TH2F * crt_xy_1 = new TH2F( TString::Format("crt_xy_1_%i", evt->get_nevent() ), TString::Format("crt_xy_1_%i", evt->get_nevent() ), 32, 0, 32, 32, 0, 32 );	
-	
+	TH2F * crt_xy_0 = new TH2F( TString::Format("crt_xy_0_%i", evt->get_nevent() ), TString::Format("crt_xy_0_%i", evt->get_nevent() ), 32, -112*8, +112*8, 32, -112*8, +112*8 );
+	TH2F * crt_xy_1 = new TH2F( TString::Format("crt_xy_1_%i", evt->get_nevent() ), TString::Format("crt_xy_1_%i", evt->get_nevent() ), 32, -112*8, +112*8, 32, -112*8, +112*8 );	
+		
 	for(int i = 0; i < crt_xy_0->GetNbinsX(); i++){
 		
 		for(int j = 0; j < crt_xy_0->GetNbinsY(); j++){
 		
-			crt_xy_0->Fill(i+1, j+1, evt->get_crt_adc(0)[i]);
-			crt_xy_0->Fill(i+1, j+1, evt->get_crt_adc(1)[j]);
+			crt_xy_0->SetBinContent( 32 - i, 32 - j, crt_xy_0->GetBinContent( 32 - i, 32 - j) + evt->get_crt_adc(0)[i]);
+			crt_xy_0->SetBinContent( 32 - i, 32 - j, crt_xy_0->GetBinContent( 32 - i, 32 - j) + evt->get_crt_adc(1)[j]);
 			
-			crt_xy_1->Fill(i+1, j+1, evt->get_crt_adc(2)[i]);
-			crt_xy_1->Fill(i+1, j+1, evt->get_crt_adc(3)[j]);
+			crt_xy_1->SetBinContent( 32 - i, 32 - j, crt_xy_1->GetBinContent( 32 - i, 32 - j) + evt->get_crt_adc(3)[i]);
+			crt_xy_1->SetBinContent( 32 - i, 32 - j, crt_xy_1->GetBinContent( 32 - i, 32 - j) + evt->get_crt_adc(2)[j]);
 
 		}
 	}
@@ -145,36 +157,41 @@ bool module_evt_display::process( event * evt){
 
 	
 	
-	c_pmt->cd(13);
+	c_pmt->cd(8);
 	crt_xy_0->Draw("COL");
-	c_pmt->cd(13)->Modified(); c_pmt->cd(13)->Update();		
+	c_pmt->cd(8)->Modified(); c_pmt->cd(8)->Update();		
 
-	c_pmt->cd(14);
+	c_pmt->cd(9);
 	crt_xy_1->Draw("COL");
-	c_pmt->cd(14)->Modified(); c_pmt->cd(14)->Update();		
+	c_pmt->cd(9)->Modified(); c_pmt->cd(9)->Update();		
 
 	
 	
 	// XY view
 	
-	//TH2F * xy_tmp = new TH2F("xy_view", "XY view; Y [mm]; X [mm]", 100, -4000, +4000, 100, -1000, +1000);
-	//	
-	//TLine * xy_line = new TLine( evt->get_crt_track_pos0()[1], evt->get_crt_track_pos0()[0], evt->get_crt_track_pos1()[1], evt->get_crt_track_pos1()[0] );
-	//
-	////xy_line->Print();
-	//
-	//c_pmt->cd(13);
-	//xy_tmp->Draw();
-	//xy_line->Draw("");
-	//c_pmt->cd(13)->Modified(); c_pmt->cd(13)->Update();		
+    TH2Poly *h2p = new TH2Poly("view_yz","view_yz", -5000,5000, -1000, 1500);
+
+    Double_t x_fc[]    = {-1500, 1500 , +1500, -1500, -1500};
+    Double_t y_fc[]    = {-500 , -500 , +500 , +500 , -500 };
+    Double_t x_crt_0[] = {+3650, +3670, +3670, +3650, +3650};
+    Double_t y_crt_0[] = {-500 , -500 , +1300, +1300, -500 };
+    Double_t x_crt_1[] = {-3670, -3650, -3650, -3670, -3670};
+    Double_t y_crt_1[] = {-500 , -500 , +1300, +1300, -500 };
+    
+    h2p->AddBin(5, x_crt_1, y_crt_1);
+	h2p->AddBin(5, x_crt_0, y_crt_0);
+	h2p->AddBin(5, x_fc, y_fc);
+	
+	TLine * yz_line = new TLine( evt->get_crt_track_pos0()[1], evt->get_crt_track_pos0()[2], evt->get_crt_track_pos1()[1], evt->get_crt_track_pos1()[2]);
+	yz_line->SetLineColor(kRed);
+	
+	c_pmt->cd(10);
+	h2p->Draw();
+	yz_line->Draw("");
+	c_pmt->cd(10)->Modified(); c_pmt->cd(10)->Update();		
     
 	// YZ view
 	
-	//TH2F * yz_tmp = new TH2F("yz_view", "YZ view; Y [mm]; Z [mm]", 100, -4000, +4000, 100, -1000, +1000);
-	//	
-	////TLine * yz_line = new TLine( - evt->get_crt_track_pos0()[1], evt->get_crt_track_param()[0] + evt->get_crt_track_pos0()[1] *  evt->get_crt_track_param()[1], evt->get_crt_track_pos1()[1], evt->get_crt_track_param()[0] - evt->get_crt_track_pos1()[1] *  evt->get_crt_track_param()[1]);
-	//
-	//TLine * yz_line = new TLine( evt->get_crt_track_pos0()[1], evt->get_crt_track_pos0()[2], evt->get_crt_track_pos1()[1], evt->get_crt_track_pos1()[2]);
 	//
 	////yz_line->Print();
 	//
@@ -211,8 +228,10 @@ bool module_evt_display::process( event * evt){
 			}
 												
 			//xy_line->Write();
-			//yz_line->Write();
 			
+			yz_line->Write();
+			h2p->Write();
+				
 			a_0->Write();
 			a_1->Write();
 			a_2->Write();
@@ -238,8 +257,8 @@ bool module_evt_display::process( event * evt){
 	//delete xy_tmp;
 	//delete xy_line;
     
-	//delete yz_tmp;
-	//delete yz_line;
+	delete h2p;
+	delete yz_line;
     
 	delete a_0;
 	delete a_1;
